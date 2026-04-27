60b15cc34b1d16ff93cec8c0694bf1fa39db0809
Aleksandr <buhfan@gmail.com>
2026-04-27 12:50:50 +0300

    Initial commit

---
+++ Controllers/MoexesController.cs
@@ -0,0 +1,58 @@
+﻿using History_DataMoex.Options;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using History_DataMoex.Parsing;
+using History_DataMoex.Clients;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace History_DataMoex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoexesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MoexOptions _moexOptions;
+        private readonly MoexHttpClient _moexHttpClient;
+
+        public MoexesController(IConfiguration configuration, MoexHttpClient moexHttpClient, IOptions<MoexOptions> moexOptions)
+        {
+            _configuration = configuration;
+            _moexHttpClient = moexHttpClient;
+            _moexOptions = moexOptions.Value;
+        }
+
+        [HttpGet("GetSecuritiesChanges")]
+        public async Task<IActionResult> GetSecuritiesChanges()
+        {
+            string url = "/calendars/stock/securities/changes.json";
+            var response = await _moexHttpClient.GetRaws(url);
+            System.IO.File.WriteAllText("stock,securities,changes.json", response);
+            MoexTableParsing parsing = new MoexTableParsing();
+            parsing.CreateTable(response);
+            Console.WriteLine();
+            return Content(response,"application/json");
+            
+            
+            
+
+        }
+        [HttpGet("GetSecuritiesBoards")]
+        public async Task<IActionResult> GetSecurities()
+        {
+            string url = "/calendars/stock/securities/boards.json";
+            var response = await _moexHttpClient.GetRaws(url);
+            System.IO.File.WriteAllText("stock,securities,boards.json", response);
+            /*MoexTableParsing parsing = new MoexTableParsing();
+            parsing.CreateTable(response);*/
+            Console.WriteLine();
+            return Content(response, "application/json");
+
+
+
+
+        }
+
+    }
+}
---
+++ Parsing/MoexTableParsing.cs
@@ -0,0 +1,54 @@
+﻿using ClosedXML.Excel;
+using System.Text.Json;
+
+namespace History_DataMoex.Parsing
+{
+    public class MoexTableParsing
+    {
+        public MoexTableParsing() 
+        {
+
+        }
+
+        public void CreateTable(string jsonstring)
+        {
+            using JsonDocument doc = JsonDocument.Parse(jsonstring);
+            var root = doc.RootElement;
+            Process(root);
+
+
+        }
+
+        void Process(JsonElement element)
+        {
+            
+
+                switch (element.ValueKind)
+                {
+                    case JsonValueKind.Object:
+                        // это объект — у него есть свойства, можно вызвать EnumerateObject()
+                        foreach (JsonProperty prop in element.EnumerateObject())
+                        {
+                            Console.WriteLine($"Ключ: {prop.Name}");
+                            Process(prop.Value);  // ← рекурсия: обрабатываем значение свойства
+                        }
+                        break;
+
+                    case JsonValueKind.Array:
+                        // это массив — у него есть элементы, можно вызвать EnumerateArray()
+                        foreach (JsonElement item in element.EnumerateArray())
+                        {
+                            Process(item);  // ← рекурсия: обрабатываем каждый элемент
+                        }
+                        break;
+
+                    default:
+                        // это лист — простое значение, дальше спускаться некуда
+                        Console.WriteLine($"Значение: {element}");
+                        break;
+                }
+                
+        }
+
+    }
+}
