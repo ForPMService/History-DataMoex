eeab3a9ffad30ea56ea17fb82fdc3660fc4df61d
Aleksandr <buhfan@gmail.com>
2026-05-02 11:29:49 +0300

    Clean for minimal api 2

--- obj/Debug/net10.0/History DataMoex.AssemblyInfo.cs
+++ obj/Debug/net10.0/History DataMoex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("History DataMoex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71cce4f6d4577868391175994ed4cbcce0c3d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7e218b02f4772f9d39131fb385f47085af2f0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("History DataMoex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("History DataMoex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
