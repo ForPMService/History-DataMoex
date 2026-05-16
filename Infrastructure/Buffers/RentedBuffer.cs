045c06d2310ea01bf3c1a5aabded1a2b661b67ed
Aleksandr <buhfan@gmail.com>
2026-05-16 13:33:49 +0300

    Create RentedBuffer

---
+++ Infrastructure/Buffers/RentedBuffer.cs
@@ -0,0 +1,54 @@
+﻿using System.Buffers;
+
+namespace History_DataMoex.Infrastructure.Buffers
+{
+    public readonly struct RentedBuffer: IDisposable
+    {
+        readonly byte[] _buffer;
+        readonly int _length;
+        
+
+        private RentedBuffer(int length, byte[] buffer)
+        {
+            _length = length;
+            _buffer = buffer;
+        }
+        public ReadOnlySpan<byte> Span => _buffer.AsSpan(0, _length);
+        public void Dispose()
+        {
+            if (_buffer != null)
+            {
+                ArrayPool<byte>.Shared.Return(_buffer);
+            }
+        }
+
+        public static async Task<RentedBuffer> RentFromStreamAsync(Stream stream, int lengthArrayFromStreamHttp, CancellationToken cancellationToken)
+        {
+            var arr = ArrayPool<byte>.Shared.Rent(lengthArrayFromStreamHttp);
+            int sizeLatsPortionByteInArray = 0;
+            int currentPositionInArray = 0;
+            try
+            {
+
+                while (currentPositionInArray < lengthArrayFromStreamHttp)
+                {
+                    sizeLatsPortionByteInArray = await stream.ReadAsync(arr, currentPositionInArray, lengthArrayFromStreamHttp - currentPositionInArray, cancellationToken);
+                    currentPositionInArray += sizeLatsPortionByteInArray;
+                    if (sizeLatsPortionByteInArray == 0)
+                    {
+                        break;
+                    }
+
+                }
+
+                return new RentedBuffer(currentPositionInArray, arr);
+            }
+            catch
+            {
+                ArrayPool<byte>.Shared.Return(arr);
+                throw;
+
+            }
+        }
+    }
+}
